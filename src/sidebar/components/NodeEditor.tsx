37d2c2cd67a10ec6580aaa1a18efb0ca15ff891c
Noyarx <64790306+Noyarx@users.noreply.github.com>
2025-10-23 18:54:50 +0200

    Added Select buttons
    + added select inputs from MUI-material lib;
    + added itemLists with some speakers and moods;
    + menu items are dinamically rendered based on itemLists;
    
    Notes:
    - I tried using <Select> from material tailwind v3 but encountered some conflicts that I couldn't resolve so I'm using MUI component now.

--- src/sidebar/components/NodeEditor.tsx
+++ src/sidebar/components/NodeEditor.tsx
@@ -1,11 +1,13 @@
 import { Input, Textarea, Typography } from "@material-tailwind/react";
-import { Stack } from "@mui/material";
-import { useEffect, useState } from "react";
+import { MenuItem, Select, Stack } from "@mui/material";
+import { useEffect, useMemo, useState, type ReactNode } from "react";
+import { moods, speakers } from "../selectItems/itemLists";
 interface NodeEditorProps {
   node: Record<string, any>;
   onChange: (updatedNode: Record<string, any>) => void;
 }
-
+const itemClass = "!mx-1.5 !my-1 !rounded";
+const itemHoverClass = "hover:!bg-slate-200";
 function NodeEditor({ node, onChange }: NodeEditorProps) {
   const graphNode = node.data;
   const [data, setData] = useState(graphNode.data);
@@ -13,7 +15,7 @@ function NodeEditor({ node, onChange }: NodeEditorProps) {
   // update editor field values on node selected change
   useEffect(() => {
     setData(graphNode.data);
-  }, [node]);
+  }, [node.id]);
 
   const handleChange = (key: string, value: any) => {
     const updatedData = { ...data, [key]: value };
@@ -21,11 +23,31 @@ function NodeEditor({ node, onChange }: NodeEditorProps) {
     onChange({
       ...node,
       data: {
-        ...node.data,
+        ...graphNode,
+        node_info: {
+          ...graphNode.node_info,
+          position: node.position,
+        },
         data: updatedData,
       },
     });
   };
+
+  const speakerItems = useMemo((): ReactNode[] => {
+    return speakers.map((e) => (
+      <MenuItem className={`${itemClass} ${itemHoverClass}`} value={e.value}>
+        {e.label}
+      </MenuItem>
+    ));
+  }, [speakers]);
+
+  const moodItems = useMemo((): ReactNode[] => {
+    return moods.map((e) => (
+      <MenuItem className={`${itemClass} ${itemHoverClass}`} value={e.value}>
+        {e.label}
+      </MenuItem>
+    ));
+  }, [moods]);
 
   // Choose what fields to render based on selected node type
   const renderNodeFields = () => {
@@ -35,30 +57,51 @@ function NodeEditor({ node, onChange }: NodeEditorProps) {
         return (
           <>
             <Stack rowGap={2}>
-              <Stack direction={"row"} columnGap={2}>
+              <Stack direction={"row"} columnGap={4}>
                 <Stack rowGap={0.5}>
-                  <label htmlFor="speaker-label">
+                  <label htmlFor="speaker">
                     <span className="">
                       <strong>Speaker:</strong>
                     </span>
                   </label>
-                  <Input
-                    color="info"
+
+                  <Select
+                    className="h-10 w-32 border rounded-md"
                     value={data.speaker || ""}
-                    onChange={(e) => handleChange("speaker", e.target.value)}
-                  />
+                    renderValue={(selected) => {
+                      if (selected.length === 0) {
+                        return <em>Speaker</em>;
+                      }
+
+                      return speakers.find((e) => e.value === selected)?.label;
+                    }}
+                    onChange={(e: any) =>
+                      handleChange("speaker", e.target.value)
+                    }
+                  >
+                    {speakerItems}
+                  </Select>
                 </Stack>
                 <Stack rowGap={0.5}>
-                  <label htmlFor="mood-label">
+                  <label htmlFor="mood">
                     <span className="">
                       <strong>Mood:</strong>
                     </span>
                   </label>
-                  <Input
-                    color="info"
+                  <Select
+                    className="h-10 w-32 border rounded-md"
                     value={data.mood || ""}
-                    onChange={(e) => handleChange("mood", e.target.value)}
-                  />
+                    renderValue={(selected) => {
+                      if (selected.length === 0) {
+                        return <em>Mood</em>;
+                      }
+
+                      return moods.find((e) => e.value === selected)?.label;
+                    }}
+                    onChange={(e: any) => handleChange("mood", e.target.value)}
+                  >
+                    {moodItems}
+                  </Select>
                 </Stack>
               </Stack>
               <Stack rowGap={0.5}>
