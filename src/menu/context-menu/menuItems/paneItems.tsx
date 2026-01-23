3c81e54799e32a8080b5467424afb9badd2d262c
Noyarx <64790306+Noyarx@users.noreply.github.com>
2026-01-23 17:33:39 +0100

    reworked addNodeMenuItems
    - removed old AddNodeMenu component;
    * grouped all menu items for adding different node types in a helper function;

--- src/menu/context-menu/menuItems/paneItems.tsx
+++ src/menu/context-menu/menuItems/paneItems.tsx
@@ -1,13 +1,6 @@
-import {
-  Add,
-  Fullscreen,
-  Tag,
-  NewReleasesRounded,
-  AltRouteRounded,
-  QuestionMark,
-  ChatOutlined,
-} from "@mui/icons-material";
+import { Add, Fullscreen } from "@mui/icons-material";
 import type { GraphActions } from "../../../editor/GraphActions.model";
+import { getAddNodeMenuItems } from "../../addNodeMenuItems";
 import type { MenuItem } from "../../models/MenuItem.model";
 
 export function getPaneContextMenuItems(
@@ -20,43 +13,7 @@ export function getPaneContextMenuItems(
       id: "submenu-add-node",
       label: "Add new node",
       icon: <Add />,
-      items: [
-        {
-          type: "action",
-          id: "action-add-node-statement",
-          label: "Statement",
-          icon: <ChatOutlined />,
-          command: () => actions.handleAddNode({ position, type: "statement" }),
-        },
-        {
-          type: "action",
-          id: "action-add-node-question",
-          label: "Question",
-          icon: <QuestionMark />,
-          command: () => actions.handleAddNode({ position, type: "question" }),
-        },
-        {
-          type: "action",
-          id: "action-add-node-condition",
-          label: "Condition",
-          icon: <AltRouteRounded className="rotate-180" />,
-          command: () => actions.handleAddNode({ position, type: "condition" }),
-        },
-        {
-          type: "action",
-          id: "action-add-node-event",
-          label: "Event",
-          icon: <NewReleasesRounded />,
-          command: () => actions.handleAddNode({ position, type: "event" }),
-        },
-        {
-          type: "action",
-          id: "action-add-node-comment",
-          label: "Comment",
-          icon: <Tag />,
-          command: () => actions.handleAddNode({ position, type: "comment" }),
-        },
-      ],
+      items: getAddNodeMenuItems({ position, actions }),
     },
     { type: "separator" },
     {
